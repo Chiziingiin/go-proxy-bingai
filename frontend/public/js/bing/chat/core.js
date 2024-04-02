2050b90948c95b9e39430e3d643aa9967953f188
Harry-zklcdc <zklcdc@qq.com>
2024-04-02 15:37:27 +0800

    [Add] 🚀 Sync latest NewBing

--- frontend/public/js/bing/chat/core.js
+++ frontend/public/js/bing/chat/core.js
@@ -14,7 +14,7 @@ _w.rms.js(
   // { 'A:rms:answers:Web:FreeSydneyHelper': '/rp/cfhgV3_HFyB3URSxigzE0QzgsF8.br.js' },
   // { 'A:rms:answers:Web:SydneyFSCHelper': '/rp/wJbTArpXX7iY8vTObyrSJ0XZMCE.br.js' },
   { 'A:rms:answers:VisualSystem:ConversationScope': '/rp/ascyKFaOzJnUTZ_D-Mv819BZ0Go.br.js' },
-  { 'A:rms:answers:CodexBundle:cib-bundle': '/rp/cmavQWvsqM-QlKupygtA4HlQd3Q.br.js' },
+  { 'A:rms:answers:CodexBundle:cib-bundle': '/rp/LV73AQ4q5yX1zii25TpIUOxTA9M.br.js' },
   { 'A:rms:answers:SharedStaticAssets:speech-sdk': '/rp/bll21ZO27j3KPE27uQBxt24c2Fw.br.js' },
   { 'A:rms:answers:Web:SydneyWelcomeScreenBase':'/rp/KgDPK_mKWjMgWe6o0SKA9dfE00Y.br.js' },
   { 'A:rms:answers:Web:SydneyWelcomeScreen':'/rp/sNzL8vfHHNmyqYOSQey9fNYd3kI.br.js' },
